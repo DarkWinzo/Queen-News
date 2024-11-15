4c011d8d477aecc23ee3e45809f29ac9890aa254
DarkWinzo <darkwinzo4440@gmail.com>
2024-11-15 17:30:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const {
 } = require("./config")
 
 async function QueenWa() {
-  const { state, saveCreds } = await useMultiFileAuthState(__dirname + "/session");
+  const { state, saveCreds } = await useMultiFileAuthState("./session");
   const conn = makeWASocket({
     auth: state,
     printQRInTerminal: true,
